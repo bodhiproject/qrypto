dc01a2664dfcc81578e1f81dfbf1ab4a549c8ad4
Deric Walintukan <deric.walintukan@gmail.com>
2018-06-27 13:48:43 +0700

    Add cases for other nav buttons

--- src/popup/components/NavBar/index.tsx
+++ src/popup/components/NavBar/index.tsx
@@ -2,18 +2,16 @@ import * as React from 'react';
 import { Typography } from '@material-ui/core';
 // import styled from 'styled-components'
 
-// import NetworkSelector from './NetworkSelector'
-// import SettingsButton from './SettingsButton'
 import BackButton from './BackButton';
+import SettingsButton from './SettingsButton';
+import NetworkSelector from './NetworkSelector';
 
-export const NavBar = ({ hasBackButton = false, title = '' }) => (
+export const NavBar = ({ hasBackButton = false, hasSettingsButton = false, hasNetworkSelector = false, title = '' }) => (
   <div style={{ margin: 8, flexDirection: 'row', display: 'inline-flex' }}>
     {hasBackButton && <BackButton />}
+    {hasSettingsButton && <SettingsButton />}
     <Typography style={{ textAlign: 'center', fontSize: 14, fontWeight: 'bold' }}>{title}</Typography>
-    {/* <TopBar>
-      <NetworkSelector />
-      <SettingsButton />
-    </TopBar> */}
+    {hasNetworkSelector && <NetworkSelector />}
   </div>
 );
 
