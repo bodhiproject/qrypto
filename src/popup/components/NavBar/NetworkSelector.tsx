75afd3f19f31564ab2324668905ec9f8b48ae39b
Deric Walintukan <deric.walintukan@gmail.com>
2018-06-28 00:04:58 +0700

    Use classes for overriding theme

--- src/popup/components/NavBar/NetworkSelector.tsx
+++ src/popup/components/NavBar/NetworkSelector.tsx
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
-import { Button } from '@material-ui/core';
+import PropTypes from 'prop-types';
+import { Button, withStyles } from '@material-ui/core';
+import { ArrowDropDown } from '@material-ui/icons';
 import { inject, observer } from 'mobx-react';
+
+import theme from '../../../config/theme';
 
 const styles = {
   networkButton: {
-    
+    minWidth: 0,
+    minHeight: 0,
+    padding: `0 ${theme.spacing.sm}`,
+    color: theme.palette.text.primary,
+    textTransform: 'none',
   },
 };
 
+@withStyles(styles, { withTheme: true })
 @inject('store')
 @observer
 export default class NetworkSelector extends Component {
+  static propTypes = {
+    classes: PropTypes.object.isRequired,
+  }
 
   public onNetworkSelectionClick = () => {
     // TODO: implement
@@ -23,10 +35,11 @@ export default class NetworkSelector extends Component {
           color="secondary"
           variant="contained"
           size="small"
-          style={styles.networkButton}
+          className={this.props.classes.networkButton}
           onClick={this.onNetworkSelectionClick}
          >
            Testnet
+           <ArrowDropDown /> 
          </Button>
       </div>
     );
