651f8ae1dd715f93b70bdd9828e1cf7204985eee
Sosho Chang <soshochang@gmail.com>
2018-07-05 14:56:14 -0700

    stylize loading page

--- src/popup/components/Loading/index.tsx
+++ src/popup/components/Loading/index.tsx
@@ -1,9 +1,13 @@
 import * as React from 'react';
+import styles from './styles';
+import { Typography, withStyles } from '@material-ui/core';
 
-const Loading = () => (
-  <div>
-    Loading...
+const Loading = (props: any) => (
+  <div className={props.classes.root}>
+    <Typography className={props.classes.text}>Loading...</Typography>
+    <div className={props.classes.anim9}>
+    </div>
   </div>
 );
 
-export default Loading;
+export default withStyles(styles)(Loading);
