3b42df364ba6dc6b7246b69023505a501d8c9735
Deric Walintukan <deric.walintukan@gmail.com>
2018-07-05 14:19:34 +0700

    Move acct section to stateless

--- src/popup/pages/Login/index.tsx
+++ src/popup/pages/Login/index.tsx
@@ -27,24 +27,24 @@ export default class Login extends Component<any, {}> {
       <div className={classes.root}>
         <Paper className={classes.headerContainer}>
           <NavBar hasNetworkSelector isDarkTheme title="Login" />
-          <div className={classes.accountContainer}>
-            <Typography className={classes.selectAcctText}>Select account</Typography>
-            <AccountSelect accounts={walletStore.accounts} />
-            <div className={classes.createAccountContainer}>
-              <Typography className={classes.orText}>or</Typography>
-              <Button className={classes.createAccountButton} color="secondary">
-                Create New Account
-              </Button>
-            </div>
-          </div>
+          <AccountSection accounts={walletStore.accounts} />
         </Paper>
       </div>
     );
   }
 }
 
-const AccountSelect = withStyles(styles, { withTheme: true })(({ classes, accounts }: any) => (
-  <Select disableUnderline className={classes.accountSelect} name="accounts">
-    {accounts.map((acct: Account) => <MenuItem value={acct.name}>{acct.name}</MenuItem>)}
-  </Select>
+const AccountSection = withStyles(styles, { withTheme: true })(({ classes, accounts }: any) => (
+  <div className={classes.accountContainer}>
+    <Typography className={classes.selectAcctText}>Select account</Typography>
+    <Select disableUnderline className={classes.accountSelect} name="accounts" value={accounts[0].name}>
+      {accounts.map((acct: Account) => <MenuItem value={acct.name}>{acct.name}</MenuItem>)}
+    </Select>
+    <div className={classes.createAccountContainer}>
+      <Typography className={classes.orText}>or</Typography>
+      <Button className={classes.createAccountButton} color="secondary">
+        Create New Account
+      </Button>
+    </div>
+  </div>
 ));
