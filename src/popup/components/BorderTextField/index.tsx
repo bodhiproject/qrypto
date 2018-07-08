69ca84be729c86dfcfec89784f17902c7392f1be
Deric Walintukan <deric.walintukan@gmail.com>
2018-07-08 17:43:13 +0700

    Rename to border text field

--- src/popup/components/BorderTextField/index.tsx
+++ src/popup/components/BorderTextField/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { TextField as _TextField, withStyles } from '@material-ui/core';
+import { TextField, withStyles } from '@material-ui/core';
 import cx from 'classnames';
 
 import styles from './styles';
 
-const TextField = withStyles(styles, { withTheme: true })(({ classes, classNames, placeholder, helperText, error, onChange }: any) => (
-  <_TextField
+const BorderTextField = withStyles(styles, { withTheme: true })(({ classes, classNames, placeholder, helperText, error, onChange }: any) => (
+  <TextField
     className={cx(classes.textField, classNames)}
     required
     type="text"
@@ -20,4 +20,4 @@ const TextField = withStyles(styles, { withTheme: true })(({ classes, classNames
   />
 ));
 
-export default TextField;
+export default BorderTextField;
