456a5a16dc3920461e38cecf99da281c14e5db9e
Deric Walintukan <deric.walintukan@gmail.com>
2018-07-24 23:41:12 +0700

    Implement balanceOf call in rpc bg

--- src/background/rpcBackground.ts
+++ src/background/rpcBackground.ts
@@ -1,6 +1,9 @@
-import { WalletRPCProvider } from 'qtumjs-wallet';
+import { WalletRPCProvider, Insight } from 'qtumjs-wallet';
+import { Contract, Decoder } from 'qweb3';
+import { find } from 'lodash';
 
 import Background from '.';
+import qrc20TokenABI from '../contracts/qrc20TokenABI';
 
 export default class RPCBackground {
   private bg: Background;
@@ -11,20 +14,38 @@ export default class RPCBackground {
     chrome.runtime.onMessage.addListener(this.onMessage);
   }
 
-  public createRpcProvider = () => {
+  public createRpcProvider = async () => {
     const wallet = this.bg.wallet.wallet;
     if (wallet) {
       this.rpcProvider = new WalletRPCProvider(wallet);
+      await this.getBalance('qMZK8FNPRm54jvTLAGEs1biTCgyCkcsmna');
     }
   }
 
-  public getBlockCount = () => {
+  public getBalance = async (balanceAddress: string) => {
+    await this.callContract('a6dd0b0399dc6162cedde85ed50c6fa4a0dd44f1', 'balanceOf', [balanceAddress]);
+  }
+
+  private callContract = async (contractAddress: string, methodName: string, args: any[]) => {
     if (this.rpcProvider) {
-      this.rpcProvider.rawCall()
+      const contract = new Contract('', contractAddress, qrc20TokenABI);
+      const methodObj = find(contract.abi, { name: methodName });
+      const dataHex = contract.constructDataHex(methodObj, args);
+
+      let res: Insight.IContractCall = await this.rpcProvider.rawCall('callContract', [
+        contract.address,
+        dataHex,
+      ]) as Insight.IContractCall;
+      res = Decoder.decodeCall(
+        res,
+        contract.abi,
+        methodName,
+      );
+      console.log(res.executionResult.formattedOutput[0].toString());
     }
   }
 
-  private onMessage = (request: any, _: chrome.runtime.MessageSender, sendResponse: (response: any) => void) => {
+  private onMessage = (request: any) => {
     switch (request.type) {
       default:
         break;
