5ae8aa445eed102d772172c65040d0cc80769d44
Sosho Chang <soshochang@gmail.com>
2018-08-23 15:34:59 -0700

    additionalEmptyWalletValidations

--- src/background/controllers/accountController.ts
+++ src/background/controllers/accountController.ts
@@ -127,6 +127,8 @@ export default class AccountController extends IController {
   * @param mnemonic The mnemonic to derive the wallet from.
   */
   public importMnemonic = async (accountName: string, mnemonic: string) => {
+    if (mnemonic === '' || mnemonic === null || mnemonic === undefined) { throw Error('invalid mnemonic'); }
+
     try {
       const network = this.main.network.network;
       const wallet = network.fromMnemonic(mnemonic);
@@ -152,6 +154,8 @@ export default class AccountController extends IController {
   * @param privateKey The private key to derive the wallet from.
   */
   public importPrivateKey = async (accountName: string, privateKey: string) => {
+    if (privateKey === '' || privateKey === null || privateKey === undefined) { throw Error('invalid privateKey'); }
+
     try {
       // recover wallet and privateKeyHash
       const network = this.main.network.network;
@@ -268,6 +272,9 @@ export default class AccountController extends IController {
   * @param privateKeyHash The private key hash to recover the wallet from.
   */
   private recoverFromPrivateKeyHash(privateKeyHash: string): QtumWallet {
+    if (privateKeyHash === '' || privateKeyHash === null
+      || privateKeyHash === undefined) { throw Error('invalid privateKeyHash'); }
+
     const network = this.main.network.network;
     return network.fromEncryptedPrivateKey(
       privateKeyHash,
